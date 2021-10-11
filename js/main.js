a011c27667900879aa8cc5cb47d84f941e6c8f78
osmaug <anar.osmanov.one@gmail.com>
2021-10-11 11:13:57 +0400

    lesson 1 update

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 const products = [
     {id: 1, title: 'Notebook', price: 2000},
-    {id: 1, title: 'Mouse', price: 20},
-    {id: 1, title: 'Keyboard', price: 200},
-    {id: 1, title: 'Gamepad', price: 50},
+    {id: 2, title: 'Mouse', price: 20},
+    {id: 3, title: 'Keyboard', price: 200},
+    {id: 4, title: 'Gamepad', price: 50},
 ];
 
 const renderProduct = product => {
     return  `<div class="product-card">
-                <img src="./img/${product.title}.png" width=200px>
-                <h3>${product.title}</h3>
-                <p>${product.price}</p>
-                <button class="buy-button">Купить</button>
+                <img class="product-card-img" src="./img/${product.title}.png" width=200px>
+                <h3 class="product-card-title">${product.title}</h3>
+                <p class="product-card-price">${product.price} USD</p>
+                <button class="buy-button"><span>Купить</span></button>
             </div>`
 }
 
 const renderPage = list => {
-    document.querySelector('.products-list').innerHTML = list.map(item => renderProduct(item)).join('');
+    document.querySelector('.products-list').innerHTML = list.map(item => renderProduct(item)).join(' ');
 }
 
 renderPage(products);
